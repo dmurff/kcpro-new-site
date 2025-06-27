1ef1a2aab0b10cf79b4fe3a58197ed7ada4b4687
David Murff <dmurffkc@gmail.com>
2025-06-26 22:30:53 -0500

    password hidden added

--- src/app/components/SignUpForm.jsx
+++ src/app/components/SignUpForm.jsx
@@ -6,8 +6,10 @@ import { BsEye } from "react-icons/bs";
 const SignUpForm = () => {
   // global styling
   const inputClass =
-    "block w-full px-4 py-2 pr-10 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500";
+    "block w-full px-4 py-2 pr-10 mb-4 border border-gray-300 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500";
   //
+
+  const [passHidden, setPassHidden] = useState(true);
 
   const [form, setForm] = useState({
     first_name: "",
@@ -122,28 +124,34 @@ const SignUpForm = () => {
         <div className="relative">
           <input
             className={inputClass}
-            type="password"
+            type={passHidden ? "password" : "text"}
             id="password"
             name="password"
             placeholder="enter password"
             value={form.password}
             onChange={changeForm}
           ></input>
-          <BsEye className="absolute top-1/2 right-3 transform -translate-y-1/2 text-blue-500 text-xl cursor-pointer" />
+          <BsEye
+            className="absolute top-1/2 right-3 transform -translate-y-1/2 text-blue-500 text-xl cursor-pointer"
+            onClick={() => setPassHidden(!passHidden)}
+          />
         </div>
 
         <label htmlFor="confirm">Confirm Password</label>
         <div className="relative">
           <input
             className={inputClass}
-            type="text"
+            type={passHidden ? "password" : "text"}
             id="confirm"
             name="confirm"
             placeholder="confirm password"
             value={form.confirm}
             onChange={changeForm}
           ></input>
-          <BsEye className="absolute top-1/2 right-3 transform -translate-y-1/2 text-blue-500 text-xl cursor-pointer" />
+          <BsEye
+            className="absolute top-1/2 right-3 transform -translate-y-1/2 text-blue-500 text-xl cursor-pointer"
+            onClick={() => setPassHidden(!passHidden)}
+          />
         </div>
         <button
           type="submit"

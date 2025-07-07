20363bcfa9334d73c0e12f4500c63d7b3323c55f
David Murff <dmurffkc@gmail.com>
2025-07-07 10:08:39 -0500

    ghost pass field fix

--- src/app/components/BaseForm.jsx
+++ src/app/components/BaseForm.jsx
@@ -1,15 +1,12 @@
 "use client";
-import { useForm } from "react-hook-form";
 
-const BaseForm = ({ children, onSubmit }) => {
+import { useForm, FormProvider } from "react-hook-form";
+
+const BaseForm = ({ onSubmit, children }) => {
   const inputClass =
     "block w-full px-4 py-2 pr-10 mb-4 border border-gray-300 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500";
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
+  const methods = useForm({
     defaultValues: {
       first_name: "",
       last_name: "",
@@ -19,57 +16,55 @@ const BaseForm = ({ children, onSubmit }) => {
   });
 
   return (
-    <section className="bg-zinc-300 p-20">
-      <form
-        className="text-black text-lg font-mono font-bold tracking-wide flex flex-col gap-4 max-w-lg w-full mx-auto p-10 bg-white my-10 rounded-md shadow-xl"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <label className="mr-4" htmlFor="first_name">
-          First Name
-        </label>
-        <input
-          className={inputClass}
-          type="text"
-          id="first_name"
-          {...register("first_name")}
-        />
-
-        <label htmlFor="last_name">Last Name</label>
-        <input
-          className={inputClass}
-          type="text"
-          id="last_name"
-          {...register("last_name")}
-        />
-
-        <label htmlFor="email">Email</label>
-        <input
-          className={inputClass}
-          type="email"
-          id="email"
-          {...register("email")}
-        />
-
-        <label htmlFor="phone">Phone</label>
-        <input
-          className={inputClass}
-          type="tel"
-          id="phone"
-          {...register("phone")}
-        />
-        {React.Children.map(children, (child) =>
-          React.isValidElement(child)
-            ? React.cloneElement(child, { register, errors })
-            : child
-        )}
-        <button
-          type="submit"
-          className="block text-white bg-blue-500 text-lg font-bold font-mono tracking-wide flex justify-center rounded-md p-3 hover:bg-blue-900"
+    <FormProvider {...methods}>
+      <section className="bg-zinc-300 p-20">
+        <form
+          className="text-black text-lg font-mono font-bold tracking-wide flex flex-col gap-4 max-w-lg w-full mx-auto p-10 bg-white my-10 rounded-md shadow-xl"
+          onSubmit={methods.handleSubmit(onSubmit)}
         >
-          Submit
-        </button>
-      </form>
-    </section>
+          <label className="mr-4" htmlFor="first_name">
+            First ame
+          </label>
+          <input
+            className={inputClass}
+            type="text"
+            id="first_name"
+            {...methods.register("first_name")}
+          />
+
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            className={inputClass}
+            type="text"
+            id="last_name"
+            {...methods.register("last_name")}
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            className={inputClass}
+            type="email"
+            id="email"
+            {...methods.register("email")}
+          />
+
+          <label htmlFor="phone">Phone</label>
+          <input
+            className={inputClass}
+            type="tel"
+            id="phone"
+            {...methods.register("phone")}
+          />
+          {children}
+          <button
+            type="submit"
+            className="block text-white bg-blue-500 text-lg font-bold font-mono tracking-wide flex justify-center rounded-md p-3 hover:bg-blue-900"
+          >
+            Submit
+          </button>
+        </form>
+      </section>
+    </FormProvider>
   );
 };
 
--- src/app/components/SignInForm.jsx
+++ src/app/components/SignInForm.jsx
@@ -1,12 +1,15 @@
 "use client";
 import { BsEye } from "react-icons/bs";
 import { useState } from "react";
+import { useFormContext } from "react-hook-form";
 
-const SignInForm = ({ register, errors }) => {
+const SignInForm = () => {
   const inputClass =
     "block w-full px-4 py-2 pr-10 mb-4 border border-gray-300 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500";
 
   const [passHidden, setPassHidden] = useState(true);
+
+  const { register, formState: errors } = useFormContext();
 
   return (
     <>
@@ -16,20 +19,20 @@ const SignInForm = ({ register, errors }) => {
           className={inputClass}
           type={passHidden ? "password" : "text"}
           id="password"
-          //   {...register("password")}
+          {...register("password")}
         />
         <BsEye
           className="absolute top-1/2 right-3 transform -translate-y-1/2 text-blue-500 text-xl cursor-pointer"
           onClick={() => setPassHidden(!passHidden)}
         />
       </div>
-      <label htmlFor="confirm">Confirm Password</label>
+      <label htmlFor="confirm">Confirm assword</label>
       <div className="relative">
         <input
           className={inputClass}
           type={passHidden ? "password" : "text"}
           id="confirm"
-          //   {...register("confirm")}
+          {...register("confirm")}
         />
         <BsEye
           className="absolute top-1/2 right-3 transform -translate-y-1/2 text-blue-500 text-xl cursor-pointer"

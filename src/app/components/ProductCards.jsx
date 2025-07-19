e5f5b1e94094ac92cb9948f0e4bb9af32db70bdd
David Murff <dmurffkc@gmail.com>
2025-07-19 09:46:53 -0500

    Deleted Component infobox

--- src/app/components/ProductCards.jsx
+++ src/app/components/ProductCards.jsx
@@ -63,7 +63,7 @@ export default function ProductCards() {
   }, []);
 
   return (
-    <div className="bg-white">
+    <div id="product-wrapper" className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Our most popular hoops
@@ -71,7 +71,7 @@ export default function ProductCards() {
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <div key={product.id} className="group relative">
+            <div key={product.id} className="group relative shadow-lg">
               <Image
                 src={product.product_images?.[0]}
                 alt={product.name}

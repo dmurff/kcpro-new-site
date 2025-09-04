d98acf543d3683842be40e1f4611c7ee510bfda6
David Murff <dmurffkc@gmail.com>
2025-09-03 21:34:56 -0500

    totalbox color changes

--- src/app/components/TotalBox.jsx
+++ src/app/components/TotalBox.jsx
@@ -3,7 +3,7 @@
 export default function TotalBox({ total }) {
   return (
     <div>
-      <h1 className="text-black text-2xl border-1 p-2 bg-green-100 border-green-500">
+      <h1 className="text-white text-2xl border-1 p-2 bg-green-700 border-green-500">
         ${total}
       </h1>
     </div>

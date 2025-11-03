81345535bdfe23fb28a12e5e2088e8c495119235
David Murff <dmurffkc@gmail.com>
2025-11-03 11:56:23 -0600

    breakpoint changes

--- src/app/components/ProductCards.jsx
+++ src/app/components/ProductCards.jsx
@@ -12,42 +12,45 @@ export default function ProductCards() {
     const fetchHoops = async () => {
       const { data, error } = await supabase
         .from("hoops")
+
         .select(
-          "id, name, brand, model, price, is_featured, feature_image, backboard_size, anchor_type, post_size, adjustment_range, install_price"
+          "id, name, brand, model, price, feature_image, backboard_size, anchor_type, post_size, adjustment_range, install_price"
         )
+        .eq("is_featured", true)
         .order("price", { ascending: true });
 
       if (error) {
         console.error("Fetch error", error);
       }
       setProducts(data);
+      console.log("🐦🐦:", data);
     };
     fetchHoops();
   }, []);
 
   return (
     <div id="product-wrapper" className="bg-white mb-24">
-      <div className="h-px bg-gray-100 w-[90%]" />
+      <div className="h-px bg-gray-100 w-[90%] mx-auto" />
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Our most popular hoops
         </h2>
 
-        <div className="mt-6 grid grid-cols-1 grid-rows-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 w-full">
+        <div className="bg-white mt-6 grid grid-cols-1 grid-rows-1 gap-x-2 gap-y-10 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8 w-[75%] mx-auto">
           {products.map((product) => (
             <div
               key={product.id}
-              className="flex items-center group relative h-[650px] w-full rounded-md border-4 shadow-xl border-transparent hover:border-orange-500 hover:shadow-xl transition-all duration-200 cursor-pointer"
+              className="flex items-center group relative h-[650px] max-w-sm sm:w-full rounded-md shadow-xl border-4 border-transparent hover:border-orange-500 hover:shadow-xl transition-all duration-500 cursor-pointer hover:scale-[1.1]"
             >
               <Link href={`/hoops/${product.id}`}>
                 <Image
                   src={product.feature_image?.[0]}
                   alt={product.name}
                   fill
-                  className="block w-full h-full object-cover object-top"
+                  className="block w-full h-full object-cover sm:object-scale-cover object-top"
                 />
               </Link>
-              <div className="absolute w-full bottom-0 mt-4 flex flex-col gap-6 justify-between bg-gray-800 p-6 rounded-t-md">
+              <div className="absolute w-full bottom-0 mt-4 flex flex-col gap-6 justify-between bg-black/10 p-6 rounded-t-md">
                 <h3 className="text-xl font-medium text-white">
                   {product.name}
                   {/* <a href={product.href}>

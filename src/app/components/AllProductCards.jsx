a496931035f68b063319f1bc1e6caa26f3f54611
David Murff <dmurffkc@gmail.com>
2025-12-01 12:12:52 -0600

    adding changes to productpage layout

--- src/app/components/AllProductCards.jsx
+++ src/app/components/AllProductCards.jsx
@@ -1,57 +1,60 @@
-"use client";
 import ProductCards from "./ProductCards";
 
 export default function AllProductCards({ products }) {
   console.log(products);
   return (
-    <section className="max-w-full mx-auto">
-      <div className="flex flex-row items-center bg-gradient-to-r from-black to-black/70 h-[500px]">
-        <div className="flex flex-col w-[50%] text-gray-300 text-center items-center justify-center p-6">
-          <div className="flex flex-col justify-items-start">
-            <h1 className="mt-2 text-4xl font-semibold tracking-tight text-white sm:text-5xl">
-              In Ground Hoops
-            </h1>
-            <p className="max-w-[250px] mt-8 text-left text-gray-300 text-base/7">
-              We offer the finest models available with unmatched curb appeal
-              and durability.
-            </p>
+    // <section className="">
+    <div className="grid grid-cols-1 lg:grid-cols-12">
+      <div className=" mx-auto lg:mx-0">
+        <div className="flex flex-row items-center bg-gradient-to-r from-black to-black/70 h-[500px] col-span-12">
+          <div className="flex flex-col w-[50%] text-gray-300 text-center items-center justify-center p-6">
+            <div className="flex flex-col justify-items-start">
+              <h1 className="mt-2 text-4xl font-semibold tracking-tight text-white sm:text-5xl">
+                In Ground Hoops
+              </h1>
+              <p className="max-w-[250px] mt-8 text-left text-gray-300 text-base/7">
+                We offer the finest models available with unmatched curb appeal
+                and durability.
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col justify-center w-[50%]">
-          <div className="rounded-md">
-            <img
-              className="rounded-md"
-              width={500}
-              src="/images/render_hoops.png"
-            />
+          <div className="flex flex-col justify-center w-[50%]">
+            <div className="rounded-md">
+              <img
+                className="rounded-md"
+                width={500}
+                src="/images/render_hoops.png"
+              />
+            </div>
           </div>
         </div>
+        <div>
+          <h2 className="text-lg text-center text-gray-900 mt-8">
+            We offer a wide range of in ground hoops for the perfect home
+            basketball experience.
+          </h2>
+        </div>
+        <div>
+          <h2 className="text-lg text-center text-gray-900 mt-8">
+            We offer a wide range of in ground hoops for the perfect home
+            basketball experience.
+          </h2>
+        </div>
+        <div>
+          <h2 className="text-lg text-center text-gray-900 mt-8">
+            We offer a wide range of in ground hoops for the perfect home
+            basketball experience.
+          </h2>
+        </div>
+        <div>
+          <h2 className="text-lg text-center text-gray-900 mt-8">
+            We offer a wide range of in ground hoops for the perfect home
+            basketball experience.
+          </h2>
+        </div>
       </div>
-      <div>
-        <h2 className="text-lg text-center text-gray-900 mt-8">
-          We offer a wide range of in ground hoops for the perfect home
-          basketball experience.
-        </h2>
-      </div>
-      <div>
-        <h2 className="text-lg text-center text-gray-900 mt-8">
-          We offer a wide range of in ground hoops for the perfect home
-          basketball experience.
-        </h2>
-      </div>
-      <div>
-        <h2 className="text-lg text-center text-gray-900 mt-8">
-          We offer a wide range of in ground hoops for the perfect home
-          basketball experience.
-        </h2>
-      </div>
-      <div>
-        <h2 className="text-lg text-center text-gray-900 mt-8">
-          We offer a wide range of in ground hoops for the perfect home
-          basketball experience.
-        </h2>
-      </div>
-    </section>
+    </div>
+    // </section>
   );
 }

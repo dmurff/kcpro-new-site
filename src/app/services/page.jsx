2604fde98216016787d9020e75b497472d720357
David Murff <dmurffkc@gmail.com>
2026-01-13 21:16:20 -0600

    force dynamic5

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+
+
 import { fetchAllServices } from "../../../lib/data/service";
 import ServiceNavbar from "../components/ServiceNavbar";
 import Footer from "../components/Footer";

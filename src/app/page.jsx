3c8f682c854d5633b417710b4f372f4f7a2e6584
David Murff <dmurffkc@gmail.com>
2026-01-14 10:55:34 -0600

    page.jsx change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,17 @@
 // import Image from "next/image";
-// "use client";
 export const dynamic = "force-dynamic";
 
 
 import Hero from "@/app/components/Hero";
-import NewBaseForm from "@/app/components/NewBaseForm";
-import LeadForm from "@/app/components/LeadForm";
 import HomepageMain from "@/app/components/HomepageMain";
 import FeaturedProducts from "@/app/components/FeaturedProducts";
 import Navbar from "@/app/components/Navbar";
-import SalesSection from "@/app/components/SalesSection";
-import HomepageCta from "@/app/components/HomepageCta";
-import HomePageOffer from "@/app/components/HomePageOffer";
 import Footer from "@/app/components/Footer";
+// import NewBaseForm from "@/app/components/NewBaseForm";
+// import LeadForm from "@/app/components/LeadForm";
+// import SalesSection from "@/app/components/SalesSection";
+// import HomepageCta from "@/app/components/HomepageCta";
+// import HomePageOffer from "@/app/components/HomePageOffer";
 
 // import Link from "next/link";
 

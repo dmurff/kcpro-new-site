e5a8203497c51c53b59c01587effbf8301d1235f
David Murff <dmurffkc@gmail.com>
2026-03-14 16:44:02 -0500

    deepgram trial

--- src/app/api/twilio/webhook/recording/route.js
+++ src/app/api/twilio/webhook/recording/route.js
@@ -11,5 +11,25 @@ export async function POST(req) {
 
   console.log(recordingUrl);
 
+  // post url to deepgram to get transcription
+
+  const url = "https://api.deepgram.com/v1/listen";
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: "Token adbc90289cfc8a33f529e9c244364d3ed15705a9",
+      "Content-Type": "application/json",
+    },
+    body: `{"url":"${recordingUrl}"}`,
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+
   return new Response("success", { status: 200 });
 }

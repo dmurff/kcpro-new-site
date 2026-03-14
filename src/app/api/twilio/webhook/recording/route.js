ef29486103b248dad55d9c2ccccc7a93e3468974
David Murff <dmurffkc@gmail.com>
2026-03-14 15:20:59 -0500

    recordingUrl

--- src/app/api/twilio/webhook/recording/route.js
+++ src/app/api/twilio/webhook/recording/route.js
@@ -5,7 +5,11 @@ export async function POST(req) {
 
   const recordingObj = Object.fromEntries(request);
 
-  console.log(recordingObj);
+  //   console.log(recordingObj);
+
+  const recordingUrl = recordingObj.RecordingUrl;
+
+  console.log(recordingUrl);
 
   return new Response("success", { status: 200 });
 }

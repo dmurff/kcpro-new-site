dcaaa3648c655d59636b04d21155fe54109bc5f2
David Murff <dmurffkc@gmail.com>
2026-03-16 12:04:56 -0500

    vercel log test 5

--- src/app/api/twilio/webhook/recording/route.js
+++ src/app/api/twilio/webhook/recording/route.js
@@ -85,41 +85,41 @@ export async function POST(req) {
   const request = await req.formData();
   const recordingObj = Object.fromEntries(request);
   const recordingUrl = recordingObj.RecordingUrl;
-  console.log("recording hit:", recordingUrl);
+  console.log(recordingUrl);
 
-  //   try {
-  //     // Step 1: Fetch audio from Twilio using your credentials
-  //     const twilioAuth = Buffer.from(
-  //       `${process.env.TWILIO_ACCOUNT_SID}:${process.env.TWILIO_AUTH_TOKEN}`,
-  //     ).toString("base64");
+  try {
+    // Step 1: Fetch audio from Twilio using your credentials
+    const twilioAuth = Buffer.from(
+      `${process.env.TWILIO_ACCOUNT_SID}:${process.env.TWILIO_AUTH_TOKEN}`,
+    ).toString("base64");
 
-  //     const audioResponse = await fetch(`${recordingUrl}.mp3`, {
-  //       headers: {
-  //         Authorization: `Basic ${twilioAuth}`,
-  //       },
-  //     });
+    const audioResponse = await fetch(`${recordingUrl}.mp3`, {
+      headers: {
+        Authorization: `Basic ${twilioAuth}`,
+      },
+    });
 
-  //     const audioBuffer = await audioResponse.arrayBuffer();
+    const audioBuffer = await audioResponse.arrayBuffer();
 
-  //     // Step 2: Send audio bytes directly to Deepgram
-  //     const response = await fetch(
-  //       "https://api.deepgram.com/v1/listen?multichannel=true&punctuate=true&model=nova-2",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           Authorization: `Token ${process.env.DEEPGRAM_API_KEY}`,
-  //           "Content-Type": "audio/mpeg",
-  //         },
-  //         body: audioBuffer,
-  //       },
-  //     );
+    // Step 2: Send audio bytes directly to Deepgram
+    const response = await fetch(
+      "https://api.deepgram.com/v1/listen?multichannel=true&punctuate=true&model=nova-2",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Token ${process.env.DEEPGRAM_API_KEY}`,
+          "Content-Type": "audio/mpeg",
+        },
+        body: audioBuffer,
+      },
+    );
 
-  //     const data = await response.json();
-  //     const transcript = data.results.channels[0].alternatives[0].transcript;
-  //     console.log(transcript);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
+    const data = await response.json();
+    const transcript = data.results.channels[0].alternatives[0].transcript;
+    console.log(transcript);
+  } catch (error) {
+    console.error(error);
+  }
 
   return new Response("success", { status: 200 });
 }

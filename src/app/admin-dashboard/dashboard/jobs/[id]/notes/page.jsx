50597b7a5f4e91122706a97cca9a983221c519d6
David Murff <dmurffkc@gmail.com>
2026-04-12 01:45:50 -0500

    added notes

--- src/app/admin-dashboard/dashboard/jobs/[id]/notes/page.jsx
+++ src/app/admin-dashboard/dashboard/jobs/[id]/notes/page.jsx
@@ -1,26 +1,71 @@
+import supabase from "../../../../../../../utils/supabaseServer";
+import GenerateNoteButton from "@/app/components/GenerateNoteButton";
+
 export default async function Notes() {
+  const { data: notes, error } = await supabase.from("notes").select("*");
+  if (error) {
+    console.log(error, error.message);
+  }
+  console.log(notes);
+
   return (
     <div>
       <div className=" max-w-7xl mx-auto p-4 bg-gray-200">
         <h1 className="mt-8 text-2xl font-semibold text-gray-900 text-center mb-8">
           Job Notes
         </h1>
-        <div className="grid grid-cols-2 bg-gray-600 w-3xl h-30 mb-2 mx-auto p-2">
-          <div className="flex gap-4 justify-center align-center">
-            <p>Customer:</p>
-            <p>Jon Gerard</p>
-          </div>
-          <div className="flex gap-4 justify-center align-center">
-            <p>Customer:</p>
-            <p>Jon Gerard</p>
-          </div>
-          <div className="flex gap-4 justify-center align-center">
-            <p>Customer:</p>
-            <p>Jon Gerard</p>
-          </div>
-          <div className="flex gap-4 justify-center align-center">
-            <p>Customer:</p>
-            <p>Jon Gerard</p>
+        <div className="flex gap-4 bg-gray-600 w-3xl mb-2 mx-auto p-2">
+          <div className="text-gray-900 bg-gray-100">
+            <div className="bg-gray-400 flex gap-4 p-4">
+              <p>Customer:</p>
+              <p>Jon Gerard</p>
+            </div>
+            {notes.map((n) => {
+              let parsed = null;
+              try {
+                parsed = JSON.parse(n.content);
+              } catch (err) {
+                parsed = null;
+              }
+              return (
+                <div className="" id={n.id} key={n.id}>
+                  <div className="flex flex-col max-h-96 p-4 overflow-y-auto border bg-black/80">
+                    {parsed ? (
+                      <>
+                        <p className="text-blue-100">
+                          Name: {parsed.customerName}
+                        </p>
+                        <p className="text-blue-100">
+                          Address: {parsed.address}
+                        </p>
+                        <p className="text-blue-100">
+                          Begin Date: {parsed.dateOfService}
+                        </p>
+                        <p className="text-blue-100">
+                          Arrival Time: {parsed.timeWindow}
+                        </p>
+                        <p className="text-blue-100">
+                          Job: {parsed.jobDescription}
+                        </p>
+                        <p className="text-blue-100">{parsed.customerType}</p>
+                        <p className="text-blue-100">
+                          Follow Up Needed:
+                          {String(parsed.followUpNeeded)}
+                        </p>
+                        <p className="text-blue-100">
+                          Details: {parsed.additionalNotes}
+                        </p>
+                      </>
+                    ) : (
+                      <p className="text-blue-100">{n.content}</p>
+                    )}
+
+                    <GenerateNoteButton n={n} />
+                  </div>
+                  <div className="border-3 border-red-500 mb-6"></div>
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="bg-gray-400 w-3xl h-30 mb-2 mx-auto"></div>
---
+++ src/app/components/GenerateNoteButton.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { formatNote } from "../../../lib/ai/formatNote";
+import { useState } from "react";
+
+export default function GenerateNoteButton({ n }) {
+  const [note, setNote] = useState("...");
+  async function handleClick() {
+    const res = await formatNote(n);
+    setNote(res);
+  }
+  return (
+    <div>
+      <button
+        onClick={handleClick}
+        className="bg-indigo-500 text-white hover:bg-indigo-700 mx-auto p-4 rounded-md m-8"
+      >
+        Generate Note
+      </button>
+      <div className="text-black bg-indigo-300">{note}</div>
+    </div>
+  );
+}

6779cf624e7aa9e1f4e9110a952281f6a49ab544
David Murff <dmurffkc@gmail.com>
2026-04-12 19:05:35 -0500

    sms webhook6

--- src/app/admin-dashboard/dashboard/jobs/[id]/sms/page.jsx
+++ src/app/admin-dashboard/dashboard/jobs/[id]/sms/page.jsx
@@ -1,7 +1,13 @@
+import supabase from "../../../../../../../utils/supabaseServer";
+
 export default async function Sms() {
+  const { data, error } = await supabase.from("messages").select("*");
+
+  console.log(data);
+
   return (
     <>
-      <div className="max-w-7xl p-6 bg-[#ededed] mx-auto"></div>
+      <div className="max-w-7xl p-6 bg-[#ededed] mx-auto">{data.body}</div>
     </>
   );
 }
--- src/app/api/twilio/webhook/sms/route.js
+++ src/app/api/twilio/webhook/sms/route.js
@@ -1,10 +1,26 @@
 import "server-only";
+import twilio from "twilio";
 import { handleSms } from "../../../../../../lib/data/handleSms.js";
 
 export async function POST(req) {
   const formData = await req.formData();
   const payload = Object.fromEntries(formData);
   console.log(payload);
+
+  const twilioSignature = req.headers.get("x-twilio-signature");
+  const url = "https://www.kcproassembly.com/api/twilio/webhook/sms";
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+  const isValid = twilio.validateRequest(
+    authToken,
+    twilioSignature,
+    url,
+    payload,
+  );
+
+  if (!isValid) {
+    return new Response("Unauthorized", { status: 403 });
+  }
 
   handleSms(payload);
 

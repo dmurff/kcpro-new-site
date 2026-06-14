c2391d90d9e0eca9a1d26811b2171b81ef0c16c1
David Murff <dmurffkc@gmail.com>
2026-06-13 23:14:57 -0500

    bg change

--- src/app/admin-dashboard/dashboard/layout.jsx
+++ src/app/admin-dashboard/dashboard/layout.jsx
@@ -4,8 +4,8 @@ export const dynamic = "force-dynamic";
 export default function DashboardLayout({ children }) {
   return (
     <>
-      <nav className="flex justify-center bg-[#ededed] p-6">
-        <ul className="flex  gap-4 lg:gap-8 max-w-4xl items-center text-lg text-gray-500 font-semibold ">
+      <nav className="flex justify-center bg-black/80 p-6">
+        <ul className="flex  gap-4 lg:gap-8 max-w-4xl items-center text-lg text-white font-semibold ">
           <li className="hover:text-gray-900 hover:cursor-pointer transition delay-100 duration-300 ease-in-out">
             <Link href="/admin-dashboard/dashboard/jobs">Jobs</Link>
           </li>

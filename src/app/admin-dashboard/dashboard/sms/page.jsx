29ac2a79acffa17376d7a8423fd4da3c6db62115
David Murff <dmurffkc@gmail.com>
2026-06-16 23:12:35 -0500

    where is the messages

--- src/app/admin-dashboard/dashboard/sms/page.jsx
+++ src/app/admin-dashboard/dashboard/sms/page.jsx
@@ -5,7 +5,10 @@ export const dynamic = "force-dynamic";
 export default async function Sms({ searchParams }) {
   const { customer, jobId } = await searchParams;
 
+  console.log("PAGE LOaded");
   console.log(jobId);
+
+  console.log(customer);
 
   const { data, error } = await supabase
     .from("messages")
